8e34cfa35bf8dff722d3cfc5c45fd92363484650
ogyre <ogyrec2019@gmail.com>
2023-07-18 08:36:55 +0300

    better

--- script.js
+++ script.js
@@ -1,143 +1,23 @@
-const resumeForm = document.getElementById("resumeForm")
-const blockField = document.getElementById('blockField')
-const event1 = resumeForm.event
-const event2 = blockField.event
+//глобальные данные второй формы
 
+//сбор данных блок-формы
+const addBlockForm = document.getElementById('addBlockForm')
+addBlockForm.onsubmit = (e) => {
+    e.preventDefault();
 
+    let formData = new FormData(addBlockForm)
 
-resumeForm.addEventListener("submit", form1Submit)
-const fullName = document.getElementById('fullName')
-//submit первой формы
-function form1Submit(e) {
-    const fullName = document.getElementById("fullName")
-    const birthYear = document.getElementById("birthYear")
-    const email = document.getElementById("email")
-
-    let isValid = true;
-
-    if (fullName.value === "") {
-        showError("fullName", "Пожалуйста, введите ФИО");
-        isValid = false;
-    } else {
-        hideError("fullName");
-    }
-
-    if (birthYear.value === "" || isNaN(birthYear.value)) {
-        showError("birthYear", "Пожалуйста, введите год рождения");
-        isValid = false;
-    } else {
-        hideError("birthYear");
-    }
-
-    if (email.value === "") {
-        showError("email", "Пожалуйста, введите почту");
-        isValid = false;
-    } else {
-        hideError("email");
-    }
-
-    if (!isValid) {
-        e.preventDefault();
-    }
-
-    const photo = document.getElementById('photo')
-    photo.value = ''
-    fullName.value = ''
-    email.value = ''
-    birthYear.value = ''
+    addBlock(e, formData)
 }
 
+//добавление дополнительного поля
 
-//Валидация первой формы
-function showError(fieldId, errorMessage) {
-    const field = document.getElementById(fieldId);
-    const testSpan = document.getElementById(`error${fieldId}`)
-    if (testSpan){
-        return
-
-    }
-    const errorSpan = document.createElement("span");
-    errorSpan.className = `error${fieldId}`;
-    errorSpan.id = `error${fieldId}`
-    errorSpan.innerHTML = errorMessage;
-
-    const existingError = field.parentNode.querySelector(`error${fieldId}`);
-    if (existingError) {
-        field.parentNode.removeChild(existingError);
-    }
-    field.parentNode.appendChild(errorSpan);
-}
-
-function hideError(fieldId) {
-    const field = document.getElementById(fieldId);
-    const existingError = field.parentNode.querySelector("error");
-    if (existingError) {
-        field.parentNode.removeChild(existingError);
-    }
-}
-
-
-//Данные из первой формы
-const data = {}
-resumeForm.addEventListener('input', e => {
-    data[e.target.name] = e.target.value
-})
-
-
-//Загрузка фото
-const inputImg = document.getElementById('photo')
-const img = document.getElementById('img')
-inputImg.onchange = e => {
-    const target = e.target
-    const fileReader = new FileReader()
-    fileReader.onload = () => {
-        img.src = fileReader.result
-    }
-    //сделать, чтобы когда ты второй раз выбераешь фото небыло тогла
-    if (img.src) {
-        img.classList.toggle('hidden')
-    }
-    fileReader.readAsDataURL(target.files[0]);
-}
-
-
-//добавляю сбор данных с input
-const userFieldData = {}
-blockField.addEventListener('input', e => {
-    userFieldData[e.target.name] = e.target.value
-})
-
-
-//Добавление блока полей
-const addFieldButton = document.getElementById('addButton')
-addFieldButton.addEventListener('click', e => {
-    const blockName = userFieldData.groupName
-
-
+const addBlock = (e, formData) => {
     const nameBlock = document.getElementById('groupName')
-    nameBlock.value = ''
     const fieldsCount = document.getElementById('fieldCounter')
-    fieldsCount.value = ''
 
-    let isValid = true;
-    console.log('value of blockName: ' + nameBlock.value)
-    if (nameBlock.value === "") {
-        showError("groupName", "Пожалуйста, введите заголовок блока");
-        isValid = false;
-    } else {
-        hideError("groupName");
-    }
-
-    if (fieldsCount.value === "" || isNaN(fieldsCount.value)) {
-        showError("fieldCounter", "Пожалуйста, введите количество заголовков");
-        isValid = false;
-    } else {
-        hideError("fieldCounter");
-    }
-
-    if (!isValid) {
-        e.preventDefault();
-    }
+    const blockName = formData.get('groupName')
+    const blockLength = formData.get('fieldCounter')
 
     const h = document.createElement('h')
     h.textContent = `${blockName}`
@@ -147,27 +27,38 @@ addFieldButton.addEventListener('click', e => {
     const parentDiv = insert.parentNode;
     parentDiv.insertBefore(h, insert);
 
-    for (let i = 0; i < userFieldData.fieldCounter; i++) {
+    for (let i = 0; i < blockLength; i++) {
         addField(blockName)
     }
-})
+    nameBlock.value = ''
+    fieldsCount.value = ''
+}
+
+
+//подгрузка фото
+const inputImg = document.getElementById('photo')
+const img = document.getElementById('img')
+inputImg.onchange = e => {
+    const target = e.target
+    const fileReader = new FileReader()
+    fileReader.onload = () => {
+        img.src = fileReader.result
+    }
+    if (img.src) {
+        img.classList.toggle('hidden')
+    }
+    fileReader.readAsDataURL(target.files[0]);
+}
 
 
 //Добавление поля
 function addField(blockName) {
-    const container = document.getElementById("addField");
-
-    const subContainer = document.createElement('div')
-    subContainer.className = 'subContainer'
-    subContainer.id = 'subContainer'
-    container.appendChild(subContainer)
-
-
     const newLabel = document.createElement("label");
     newLabel.textContent = "Заголовок поля";
     const newInput = document.createElement("input");
     newInput.className = `${blockName}`
     newInput.id = '0'
+    newInput.name = `${blockName}`
 
 
     const newLabel1 = document.createElement("label");
@@ -175,6 +66,7 @@ function addField(blockName) {
     const newInput1 = document.createElement("input");
     newInput1.className = `${blockName}`
     newInput1.id = '1'
+    newInput1.name = `${blockName}`
 
     const insert = document.getElementById('addFieldsButton')
     const parentDiv = insert.parentNode;
@@ -187,78 +79,91 @@ function addField(blockName) {
 }
 
 
-const form = document.getElementById("addField");
-form.addEventListener("submit", form1Submit.bind(event1))
-form.addEventListener("submit", form2Submit)
-const allUserCreatedFieldsData = {}
+//Сбор данных главной формы
+const mainForm = document.getElementById('mainForm')
+mainForm.onsubmit = (e) => {
+    e.preventDefault();
 
-//сбор данных с дополнительной формы
-let fieldValues = []
-const blockNamesWithLength = new Map()
+    let formData = new FormData(mainForm)
+    // for (let [name, value] of formData) {
+    //     console.log(`${name} = ${value}`);
+    // }
 
-function form2Submit() {
+    resumeGenerator(formData)
 
-    const inputs = form.querySelectorAll("input");
-    const formData = {};
-    let fieldsArr = []
-    let counter = 1
-    inputs.forEach(function (input, i) {
+}
 
-        if (blockNamesWithLength.get(input.className) !== undefined) {
-            blockNamesWithLength.set(input.className, counter + 1)
-            counter++
-        } else {
+
+//resume generator
+const resumeGenerator = (formData) => {
+    mainFillResume(formData)
+    formData.delete('fullName')
+    formData.delete('birthYear')
+    formData.delete('email')
+    formData.delete('photo')
+    for (let [name, value] of formData) {
+        console.log(`${name} = ${value}`);
+    }
+    subFillResume(formData)
+
+}
+
+//заполнение основных данных
+function mainFillResume(data) {
+
+    const fullNameR = document.getElementById('fullNameR')
+    fullNameR.textContent = data.get('fullName')
+
+    const birthYearR = document.getElementById('birthYearR')
+    birthYearR.textContent = data.get('birthYear')
+
+    const emailR = document.getElementById('emailR')
+    emailR.textContent = data.get('email')
+    //img
+    // const imgR = document.getElementById('imgR')
+    // imgR.src = `${data.photo}`
+}
+
+
+//заполнение новых данных
+function subFillResume(formData) {
+    const map = new Map()
+    const arr = []
+    let counter = 0
+    for (let [key, value] of formData) {
+        if (map.get(key) === undefined) {
+            map.set(key, 1)
             counter = 1
-            blockNamesWithLength.set(input.className, counter)
+        } else {
+            counter++
+            map.set(key, counter)
         }
 
-        fieldsArr.push(input.value)
-        formData[i] = input.value
+        arr.push(value)
+    }
+    console.log(map)
+    const splitArr = splitArrayIntoPairs(arr)
+    console.log(splitArr)
 
-        allUserCreatedFieldsData[input.className] = formData
-    });
-
-    fieldValues = splitArrayIntoPairs(fieldsArr)
-
-
-    //здесь описана та самая функция, которая выводит блоки с
     const insert = document.getElementById('resume')
     const parentDiv = insert.parentNode;
-    console.log(blockNamesWithLength)
-    for (let key of blockNamesWithLength.keys()) {
+
+    for (let key of map.keys()) {
         const blockName = document.createElement('div')
         blockName.textContent = key
         parentDiv.insertBefore(blockName, insert)
-        const length = blockNamesWithLength.get(key) / 2
+        const length = map.get(key) / 2
         for (let i = 0; i < length; i++) {
             const element = document.createElement('div')
-            element.textContent = `- ${fieldValues[i][0]}: ${fieldValues[i][1]}`
+            element.textContent = `- ${splitArr[i][0]}: ${splitArr[i][1]}`
             parentDiv.insertBefore(element, insert);
         }
+        splitArr.splice(0, length)
     }
-    fillResume()
+
 }
 
 
-
-//заполнение основного резюме
-function fillResume() {
-
-    const fullNameR = document.getElementById('fullNameR')
-    fullNameR.textContent = data.fullName
-
-    const birthYearR = document.getElementById('birthYearR')
-    birthYearR.textContent = data.birthYear
-
-    const emailR = document.getElementById('emailR')
-    emailR.textContent = data.email
-    //img
-    const imgR = document.getElementById('imgR')
-    imgR.src = `${data.photo}`
-}
-
-
-//разбиение массива на пары
 function splitArrayIntoPairs(array) {
     const pairs = [];
 
@@ -269,3 +174,4 @@ function splitArrayIntoPairs(array) {
 
     return pairs;
 }
+
